cf9a8a34860d1269d7c9e9a772f94b090b1696b9
francescocifaldi <francesco.cifaldi@gmail.com>
2025-02-25 21:49:42 +0100

    ADD CheckGuest with for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,29 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci il tuo nome:");
+        String name = scanner.nextLine();
+        boolean isGuest = false;
 
+        // using for cicle
+        for (int i = 0; i < guests.length; i++) {
+            if (name.equals(guests[i])) {
+                isGuest = true;
+                break;
+            }
+        }
+
+        if (isGuest) {
+            System.out.println("Benvenuto alla festa" + name + "!");
+        } else {
+            System.out.println("Mi dispiace, " + name + " non sei in lista.");
+        }
+        scanner.close();
     }
 }
